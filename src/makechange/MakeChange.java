31f8c8601f34b2995b302c12225a61682c3b6ee6
JustLiving <JustinTLivingston@gmail.com>
2020-08-28 15:58:03 -0700

    Started first 2 user stories

--- src/makechange/MakeChange.java
+++ src/makechange/MakeChange.java
@@ -1,7 +1,23 @@
 package makechange;
 
+import java.util.Scanner;
+
 public class MakeChange {
 	public static void main(String[] args) {
-
+		Scanner kb = new Scanner(System.in);
+		//User Story #1
+		System.out.print("What is the price of the item: ");
+		double price = kb.nextDouble();
+		//User Story #2
+		System.out.print("The amount tendered was: ");
+		double tender = kb.nextDouble();
+		
+		//User Story #3
+//		Display an appropriate message if the customer 
+//		provided too little money or the exact amount.
+		
+		
+		
+		kb.close();
 	}
 }
